745d4c5cd294fd2b9336d6de3801e2784e892f2e
Aanand Kumar <152170009+Aanand251@users.noreply.github.com>
2026-07-09 13:22:36 +0530

    Time: 8 ms (27.84%), Space: 43.1 MB (72.5%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -7,15 +7,16 @@ class Solution {
         for(int i = 0; i<n ; i++){
             map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) , 0)+1);
         }
-       for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-           int freq = entry.getValue();
-           if (freq % 2 == 0) {
-              len += freq;
-         } else {
-        len += freq - 1;
-        odd = true;
-    }
-}    
+        for(Character key : map.keySet()){
+            
+         if (map.get(key) % 2 == 0 ) {
+             len += map.get(key);
+        }
+          else {
+          len += map.get(key) - 1 ;
+          odd = true;
+         }
+      }
      if(odd){
     len++;
    }
