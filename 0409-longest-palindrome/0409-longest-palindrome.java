564c5bfc61f3f7032b7c75bb30b1deaafda2e36f
Aanand Kumar <152170009+Aanand251@users.noreply.github.com>
2026-07-09 13:21:51 +0530

    Time: 8 ms (27.84%), Space: 43.2 MB (57.44%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -3,19 +3,19 @@ class Solution {
         int n = s.length();
         int len = 0;
         boolean odd = false ;
-        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>(64);
         for(int i = 0; i<n ; i++){
             map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) , 0)+1);
         }
-        for(Character key : map.keySet()){
-        int freq = map.get(key);
-        if (freq % 2 == 0) {
-          len += freq;
+       for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+           int freq = entry.getValue();
+           if (freq % 2 == 0) {
+              len += freq;
          } else {
-          len += freq - 1;
-          odd = true;
-     }
-      }
+        len += freq - 1;
+        odd = true;
+    }
+}    
      if(odd){
     len++;
    }
