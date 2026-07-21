04da57e7fc070febec4ea3ca5093006ddf64c431
Anand Choudhary <152170009+Aanand251@users.noreply.github.com>
2026-07-21 14:23:38 +0530

    Time: 38 ms (15.71%), Space: 48.3 MB (84.57%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int distributeCandies(int[] a) {
+        int n = a.length;
+        int limitation = n/2;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i:a){
+         map.put(i , map.getOrDefault(i, 0)+1);
+        }
+        int unique = map.size();
+
+        return Math.min(unique, limitation);
+    }
+}
