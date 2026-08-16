7645cc187a6b94914b9009b35d96ec5556b8c71e
Anand Choudhary <152170009+Aanand251@users.noreply.github.com>
2026-08-16 17:13:48 +0530

    Time: 4 ms (53.69%), Space: 43.1 MB (38.43%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -0,0 +1,67 @@
+class Solution { 
+    class pair { 
+        char letter; 
+        int freq; 
+
+        pair(char letter, int freq) { 
+            this.letter = letter; 
+            this.freq = freq; 
+        } 
+    } 
+
+    public String reorganizeString(String s) { 
+        HashMap<Character, Integer> map = new HashMap<>(); 
+
+        Comparator<pair> comparator = (a, b) -> { 
+            return b.freq - a.freq; 
+        }; 
+
+        PriorityQueue<pair> pq = new PriorityQueue<>(comparator); 
+
+        for(int i = 0; i < s.length(); i++) { 
+            char c = s.charAt(i); 
+            map.put(c, map.getOrDefault(c, 0) + 1); 
+        } 
+
+        StringBuilder sb = new StringBuilder(); 
+
+        for(Map.Entry<Character, Integer> entry : map.entrySet()) { 
+            pq.offer(new pair(entry.getKey(), entry.getValue())); 
+        } 
+
+        while(pq.size() >= 2) { 
+            pair firstpoll = pq.poll(); 
+            pair secondpoll = pq.poll(); 
+
+            sb.append(firstpoll.letter); 
+            sb.append(secondpoll.letter); 
+
+            firstpoll.freq--; 
+            secondpoll.freq--; 
+
+            if(firstpoll.freq > 0) { 
+                pq.add(firstpoll); 
+            } 
+
+            if(secondpoll.freq > 0) { 
+                pq.add(secondpoll); 
+            } 
+        } 
+
+        if(!pq.isEmpty()) { 
+            pair lastpoll = pq.poll(); 
+
+            if(lastpoll.freq > 1) { 
+                return ""; 
+            } 
+
+            if(sb.length() > 0 && sb.charAt(sb.length() - 1) == lastpoll.letter) { 
+                return ""; 
+            } 
+
+            sb.append(lastpoll.letter); 
+        } 
+
+        return sb.toString(); 
+    } 
+}
